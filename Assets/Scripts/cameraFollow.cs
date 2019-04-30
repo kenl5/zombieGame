2aa2da2de0dbb12f67ed2f8d743591220649fe67
kenl5 <kenl5@uci.edu>
2019-04-30 02:10:34 -0700

    camera follow and more complete map.

---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform player;
+
+    public float speed = 0.05f;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        transform.position = player.position + offset;
+    }
+
+}
